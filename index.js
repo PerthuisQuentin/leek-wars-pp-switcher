efd7e38a3b070d2643f856470481e46c30cbcd01
PerthuisQuentin <perthuis.quentin@gmail.com>
2019-12-03 00:56:59 +0100

    Update code

--- index.js
+++ index.js
@@ -1,63 +1,73 @@
-const Fs = require('fs');
-const Path = require('path');
-const Request = require('request');
-const Restler = require('restler');
+const Fs = require('fs')
+const Path = require('path')
 
-const config = JSON.parse(Fs.readFileSync('./config.json'));
+const Request = require('request')
 
-const imgDir = config.imgDir;
-const username = config.username;
-const password = config.password;
+const config = JSON.parse(Fs.readFileSync('./config.json'))
 
+const randomInt = (low, high) => Math.floor(Math.random() * (high - low) + low)
 
 // Read the /img directory
-Fs.readdir(imgDir, (err, files) => {
-    if(err) return console.log(err);
+const chooseAvatar = path => new Promise((resolve, reject) => {
+	Fs.readdir(path, (error, files) => {
+		if(error) return reject(error)
+	
+		const images = files.filter(file => ['.png', '.jpg'].includes(Path.extname(file)))
+	
+		if(!images.length) return reject('/img directory does not contains images.')
+		
+		const randIndex = randomInt(0, images.length)
+		const choosenFile = images[randIndex]
+	
+		console.log('New avatar selected : ' + choosenFile)
+	
+		resolve(path + choosenFile)
+	})
+})
 
-    files = files.filter(element => {
-		return ['.png', '.jpg'].includes(Path.extname(element));
-	});
-
-    if(!files.length) return console.log('/img directory contains no images.');
-    
-    var randFile = randomInt(0, files.length);
-    console.log('PP will be switch with : ' + files[randFile]);
-
-    updateAvatar(imgDir + files[randFile]);
-});
-
-// Connect to Leek Wars and update the avatar
-function updateAvatar(filePath) {
-	// Retrieve token
-	Request({
-		url: 'https://leekwars.com/api/farmer/login-token/' + username + '/' + password
-	}, (error, response, body) => {
-		if(error) {
-			console.log(error);
-			return;
+const getLeekWarsToken = (username, password) => new Promise((resolve, reject) => {
+	const options = {
+		method: 'GET',
+		url: `https://leekwars.com/api/farmer/login-token/${username}/${password}`,
+		headers: {
+			'Content-Type': 'application/json'
 		}
-		if(response.statusCode != 200) {
-			console.log(response);
-			return;
+	}
+	
+	Request(options, (error, result, body) => {
+		if (error) reject(error)
+		resolve(JSON.parse(body).token)
+	})
+})
+
+const updateLeekWarsAvatar = (token, image) => new Promise((resolve, reject) => {
+	const options = {
+		method: 'POST',
+		url: 'https://leekwars.com/api/farmer/set-avatar/avatar',
+		headers: {
+			'Authorization': `Bearer ${token}`,
+			'Content-Type': 'multipart/form-data'
+		},
+		formData : {
+			'avatar' : Fs.createReadStream(image)
 		}
+	}
+	
+	Request(options, (error, result, body) => {
+		if (error) reject(error)
+		resolve(body)
+	})
+})
 
-		var token = JSON.parse(body).token;
-
-		// Update avatar
-		Fs.stat(filePath, (err, stats) => {
-		    Restler.post('https://leekwars.com/api/farmer/set-avatar/avatar', {
-		        multipart: true,
-		        data: {
-		            "token": token,
-		            "avatar": Restler.file(filePath, null, stats.size, null, "image/png")
-		        }
-		    }).on("complete", data => {
-		        console.log(data);
-		    });
-		});
-	});
-}
-
-function randomInt(low, high) {
-    return Math.floor(Math.random() * (high - low) + low);
-}
+Promise.all([
+	chooseAvatar(config.imageDirectory),
+	getLeekWarsToken(config.username, config.password)
+])
+	.then(result => {
+		const [image, token] = result
+		return updateLeekWarsAvatar(token, image)
+	})
+	.then(() => {
+		console.log('Success')
+	})
+	.catch(console.error)
